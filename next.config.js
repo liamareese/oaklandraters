c15cd8c87187b7c19640326af68e2fff4d885812
liamareese <liamareese@gmail.com>
2025-07-27 23:10:09 -0700

    updating

--- next.config.js
+++ next.config.js
@@ -2,14 +2,7 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  // Example: Add a basePath if your app is not at the root
-  // basePath: '/your-app-base-path',
+  output: 'export', // <-- Add this line
 };
 
-// added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
-// Converted from import to require for CommonJS compatibility in next.config.js
-const { initOpenNextCloudflareForDev } = require('@opennextjs/cloudflare');
-initOpenNextCloudflareForDev();
-
-module.exports = nextConfig;
+export default nextConfig;
