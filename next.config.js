da2ede195e9848e0f18299430fc8e6ad9b884868
liamareese <liamareese@gmail.com>
2025-07-28 01:56:09 -0700

    update for pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',          // tell Next.js to do a static export
 };
-
 module.exports = nextConfig;
